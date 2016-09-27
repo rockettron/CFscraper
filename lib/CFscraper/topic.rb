d1e9c9ae970b4ef106d2a06a8b767d905437ecb8
RDimon2912 <rdimon2912@gmail.com>
2016-09-27 03:30:00 +0300

    Worked version

--- lib/CFscraper/topic.rb
+++ lib/CFscraper/topic.rb
@@ -3,46 +3,57 @@ require_relative "helper"
 module CFscraper
     class Topic < CFscraper::Scraper
 
-    	def self.attributes
-    		%w{
-    			id
-    			title
+        def initialize(url_or_dom)
+            if url_or_dom.class.name == "String"
+                super(url_or_dom)
+                upd_topic
+            else
+                @dom = url_or_dom
+            end
+            @attributes = %w{
+                id
+                title
                 author
                 time_create
                 content
-    		}    	
-    	end
-
-        def initialize(node_dom)
-            @node = node_dom
+            } 
         end
 
     	def id
-    		@node['topicid'].to_i
+    		@id ||= @dom['topicid'].to_i
     	end
 
     	def title
-    		@node.at_css('.title a').text.strip
+    		@title ||= @dom.at_css('.title a').text.strip
     	end
 
         def author
-            @node.at_css('.info a').text.strip
+            @author ||= @dom.at_css('.info a').text.strip
         end
 
         def time_create
-            @node.at_css('.format-humantime')['title']
+            @time_create ||= @dom.at_css('.format-humantime')['title']
         end
 
         def content
-            node = @node.at_css('.ttypography').text
+            @content ||= @dom.at_css('.ttypography').text
+        end
+
+        def upd_topic
+            @dom = @dom.at_css(".topic")
         end
     	
     end
 
     class Content < CFscraper::Scraper
 
+        def initialize(*attr)
+            super(*attr)
+            @attributes = %w{ topics }
+        end
+
         def topics
-            @dom.css('.topic').map { |topic| Topic.new(topic).to_hash }
+            @topics ||= @dom.css('.topic').map { |topic| Topic.new(topic).to_hash }
         end
 
     end
